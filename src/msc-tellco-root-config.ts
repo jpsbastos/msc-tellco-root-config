f3a6bb540c58d39eda75dc72e5241c9280609d96
João Bastos <ctw00887@criticaltechworks.com>
2020-06-14 23:11:43 +0100

    Fixed linting

--- src/msc-tellco-root-config.ts
+++ src/msc-tellco-root-config.ts
@@ -1,16 +1,37 @@
-import { registerApplication, start } from 'single-spa';
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from 'single-spa-layout';
-const routes = constructRoutes(document.querySelector('#single-spa-layout'));
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
+import { registerApplication, start } from "single-spa";
+// import {
+//   constructApplications,
+//   constructRoutes,
+//   constructLayoutEngine,
+// } from "single-spa-layout";
+//
+// const htmlTemplate: HTMLElement = document.querySelector("#single-spa-layout");
+// const routes = constructRoutes(htmlTemplate);
+// const applications = constructApplications({
+//   routes,
+//   loadApp({ name }) {
+//     return System.import(name);
+//   },
+// });
+// const layoutEngine = constructLayoutEngine({ routes, applications });
+// applications.forEach(registerApplication);
+
+registerApplication({
+  name: "@msc-tellco/navbar",
+  app: () => System.import("@msc-tellco/navbar"),
+  activeWhen: "/",
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
-applications.forEach(registerApplication);
+
+registerApplication({
+  name: "@msc-tellco/catalogue",
+  app: () => System.import("@msc-tellco/catalogue"),
+  activeWhen: "/",
+});
+
+registerApplication({
+  name: "@msc-tellco/shopping-cart",
+  app: () => System.import("@msc-tellco/shopping-cart"),
+  activeWhen: "/cart",
+});
+
 start();
